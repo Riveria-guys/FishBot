86fef4f3a29961ed6fafe6b75a57637de8f9219b
test1 <test1@test.com>
2025-02-18 09:00:42 +0200

    Добавил обработку сообщений в группе и в личке и добавил API key (если кто-то его спиздит получите пизды)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 
 # Инициализация бота с использованием токена
 # Замените 'YOUR_BOT_TOKEN' на токен вашего бота
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = "8099645779:AAHlgE9Bi36Xek7RssuHrFur3zEPbIpuKP8"
 bot = telebot.TeleBot(TOKEN)
 
 # Обработчик команд /start и /help
@@ -11,11 +11,14 @@ def send_welcome(message):
     """Отправляет приветственное сообщение пользователю."""
     bot.reply_to(message, "Привет! Я твой бот. Чем могу помочь?")
 
-# Обработчик всех входящих сообщений
+# Обработчик всех входящих сообщений в личке и группах
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    """Эхо-функция, повторяет полученное сообщение."""
-    bot.reply_to(message, message.text)
+def handle_message(message):
+    """Обрабатывает сообщения в личных чатах и группах."""
+    if message.chat.type == "private":
+        bot.reply_to(message, "Ты пишешь мне в личку!")
+    elif message.chat.type in ["group", "supergroup"]:
+        bot.reply_to(message, f"Сообщение в группе: {message.text}")
 
 # Запуск бота в режиме непрерывного прослушивания сообщений
 print("Бот запущен...")
