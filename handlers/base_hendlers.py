2c661ab022f34d01d3feaf2d699e7a79bd14c376
Hyp3rGam3r <mykola.stankov07@gmail.com>
2025-03-03 13:35:49 +0200

    DEV-22: added handlers

--- handlers/base_hendlers.py
+++ handlers/base_hendlers.py
@@ -1,1 +1,16 @@
 #zdes obrobotka vseh zaprosov
+import telebot
+
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    """Отправляет приветственное сообщение пользователю."""
+    bot.reply_to(message, "Привет! Я твой бот. Чем могу помочь?")
+
+
+# Обработчик команды /help
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    """Отправляет список команд пользователю."""
+    bot.reply_to(message, "Тебе тут никто не поможет, сам разбирайся.\nВот список команд: \n/start - стартуем бота\n/help - список команд\n/msp - магический шар")
