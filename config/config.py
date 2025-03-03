e1442c2a1b4812cb7e86070ebe33fda8faf58a52
Fuokushi <lioness.yarush@gmail.com>
2025-03-03 16:34:47 +0200

    Реализовал загрузку конфигурации

--- config/config.py
+++ config/config.py
@@ -4,3 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+BOT_TOKEN = os.getenv("TOKEN")
+if BOT_TOKEN is None:
+    print("Ошибка: токен не найден!")
+else:
+    print("Токен успешно загружен.")
